a235e4a4568470a1937d0616bca81cffc3218682
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-06-04 09:23:26 +0300

    chore: Add new task screen design and functionality

--- todo_app/lib/screens/add_new_task.dart
+++ todo_app/lib/screens/add_new_task.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:todo_app/constants/color.dart';
 
 class AddNewTaskScreen extends StatelessWidget {
   const AddNewTaskScreen({super.key});
@@ -9,6 +11,7 @@ class AddNewTaskScreen extends StatelessWidget {
     double deviceWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
+        backgroundColor: HexColor(backgrouncolor),
         body: Column(
           children: [
             Container(
@@ -22,7 +25,91 @@ class AddNewTaskScreen extends StatelessWidget {
                   fit: BoxFit.cover,
                 ),
               ),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon:
+                        const Icon(Icons.close, size: 35, color: Colors.white),
+                  ),
+                  const Expanded(
+                    child: Text(
+                      "Add New Task",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
             ),
+            const Text("Task Title"),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 20,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Enter Task Title",
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text("Category"),
+                GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        duration: Duration(seconds: 1),
+                        content: Text("Category Selected"),
+                      ),
+                    );
+                  },
+                  child: Image.asset("lib/assets/images/category_1.png"),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        duration: Duration(seconds: 1),
+                        content: Text("Category Selected"),
+                      ),
+                    );
+                  },
+                  child: Image.asset("lib/assets/images/category_2.png"),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        duration: Duration(seconds: 1),
+                        content: Text("Category Selected"),
+                      ),
+                    );
+                  },
+                  child: Image.asset("lib/assets/images/category_3.png"),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [Text("Date"), TextField()],
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
